811280c74e1c8057ed62b371dae7c7216879663a
Khalil <Khalilbentaiba@gmail.com>
2023-01-10 18:27:00 +0100

    Add clear screen when clicking operator then a number

--- script.js
+++ script.js
@@ -17,22 +17,55 @@ function addToScreen(num) {
     screen.textContent += num;
 }
 
+function operate(numbers, operator) {
+    if (numbers.length === 1) screen.textContent = numbers[0];
+    else if (numbers.length === 0) screen.textContent = "";
+    else {
+        switch (operator) {
+            case '+': screen.textContent = numbers[0] + numbers[1];
+            case '-': screen.textContent = numbers[0] - numbers[1];
+            case 'x': screen.textContent = numbers[0] * numbers[1];
+            case '/':
+                if (numbers[1] === 0) screen.textContent = 'Math Error';
+                else screen.textContent = numbers[0] / numbers[1];
+        }
+    }
+}
+
 const screen = document.querySelector('.screen');
 const numbers = document.querySelectorAll('.number');
 const functions = document.querySelectorAll('.func');
+let funcClick = false;
+let x = 0;
+let y = null;
 
 functions.forEach(func => {
-    switch (func.textContent) {
-        case 'CE': func.addEventListener('click', CE);
-        case 'C' : func.addEventListener('click', clear);
-        case 'Delete': func.addEventListener('click', Delete);
-    }
-    
+    if (func.textContent === 'CE') func.addEventListener('click', CE);
+    else if (func.textContent === 'C') func.addEventListener('click', clear);
+    else if (func.textContent === 'Delete') func.addEventListener('click', Delete);
+    else {
+        func.addEventListener('click', () => funcClick = true);
+    };
 });
 
 numbers.forEach(number => {
     number.addEventListener('click', () => {
-        addToScreen(number.textContent);
+        if (number.textContent === '+/-') {
+            if (screen.textContent !== "") {
+                screen.textContent = - screen.textContent;
+            }
+        } else if (number.textContent === '.') {
+            if (!(screen.textContent.includes('.'))) {
+                screen.textContent += '.';
+            }
+        } else {
+            if (funcClick) {
+                CE()
+                funcClick = false;
+            }
+            addToScreen(number.textContent);
+        }
+        
     });
 });
 
